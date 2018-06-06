44c71ffaeb9a5525e1070e0f40a84fed2802fb7e
Rafael Schouten <rafaelschouten@gmail.com>
2018-06-06 23:56:13 +1000

    back in git

--- src/functions.jl
+++ src/functions.jl
@@ -1,93 +1,28 @@
 # Library of common DEB functions.
 
-export find_rate, tempcorr, save_rate!, catabolic_fluxes, stoich_merge, synthesizing_unit
+"""
+    tempcorr(T, T1, A, [L, AL,] [H, AH])
+DEB tempcorr function. Uses lower and uppper bounds if they are supplied.
+Temperatures all in Kelvins.
+"""
+tempcorr(t, tc) = 1.0
+tempcorr(t, tc::AbstractTempCorr) = tempcorr(t |> u"K", getfield.(tc, fieldnames(tc))...)
+tempcorr(t, t1, a) = exp(a/t1 - a/t)
+tempcorr(t, t1, a, l, al) = 
+    exp(a/t1 - a/t) * (1.0 + exp(al/t1 - al/l)) / (1.0 + exp(al/t - al/l))
+tempcorr(t, t1, a, l, al, h, ah) = 
+    exp(a/t1 - a/t) * (1.0 + exp(al/t1 - al/l) + exp(ah/h - ah/t1)) / 
+    (1.0 + exp(al/t - al/l) + exp(ah/h - ah/t))
 
 """
-    area_mass_kooijman(uV, Vref, Vscaling)
-Area mass scaling for plants.
-(uV / Vref)^(1/3 - (-uV / Vscaling)^b) DEB book p. 134
-"""
-function area_mass_kooijman(uV::Float64, Vref::Float64, Vscaling::Float64)::Float64
-    uV > 0.0 || return 0.0
-    (uV / Vref)^(-uV / Vscaling)
-end
-
-function area_mass_linear(uV::Float64, Vref::Float64, Vscaling::Float64)::Float64
-    return 1.0
-end
-
-"""
-tempcorr(T, T1, A)
-"""
-function tempcorr(T, T1, A)
-    exp(A/T1 - A/T)
-end
-function tempcorr(T, T1, A, L, AL)
-    exp(A/T1 - A/T) * (1.0 + exp(AL/T1 - AL/L)) / (1.0 + exp(AL/T - AL/L))
-end
-function tempcorr(T, T1, A, L, AL, H, AH)
-    exp(A/T1 - A/T) * (1.0 + exp(AL/T1 - AL/L) + exp(AH/H - AH/T1)) / 
-    (1.0 + exp(AL/T - AL/L) + exp(AH/H - AH/T))
-end
-
-"""
-    find_rate(t, rates::Array{Float64}, args)
-Calculate rate formula.
-
-TODO: use Roots.jl for this
-"""
-function find_rate(t, rates::Array{Float64}, 
-                   args::Tuple{NTuple{N,Float64},NTuple{N,Float64},Vararg{Float64,V}})::Float64 where {N,V}
-    local f = rate_formula
-    local found = false
-
-    # Find the largest possible rate window.
-    x0, x1 = rate_window(args...) 
-
-    # Find root with the secant method.
-    y0 = f(x0, args...)
-    y1 = f(x1, args...)
-    i = 1
-    local x::Float64
-    for _ in 1:BI_MAXITER
-        x = x1 - y1 * (x1 - x0) / (y1 - y0)
-        if abs(x-x1) < BI_XTOL
-            found = true
-            break
-        end
-        x0 = x1
-        y0 = y1
-        x1 = x
-        y1 = f(x1, args...)
-    end
-
-    if found
-        # Save rate for plotting.
-        save_rate!(t, rates, x)
-        return x
-    else
-        error("Root not found for args: $args")
-    end
-end
-
-function save_rate!(t::Float64, rates::Array{Float64}, newrate::Float64)
-    t_floor = floor(Int64, t)
-    if t_floor == t
-        r = rates[t_floor + 1] = newrate
-    end
-end
-
-"""
-Get window of possible rates with CN or CNE rserves
+Get bracket of possible rates with CN or CNE rserves
 Calculated for y_E_CH_NO and y_E_EN -> ∞
 """
-function rate_window(ureserve::NTuple{2,Float64}, A_turnover::NTuple{2,Float64}, 
-                     args::Vararg)::NTuple{2,Float64}
-    rate_window((ureserve[1], ureserve[2], 0.0), (A_turnover[1], A_turnover[2], 0.0), args...)
+function rate_bracket(ureserve::NTuple{2}, A_turnover::NTuple{2}, args::Vararg)::NTuple{2}
+    rate_bracket((ureserve[1], ureserve[2], 0.0), (A_turnover[1], A_turnover[2], 0.0), args...)
 end
-function rate_window(ureserve::NTuple{3,Float64}, A_turnover::NTuple{3,Float64}, 
-                     j_E_mai::Float64, y_E_CH_NO::Float64, y_E_EN::Float64, 
-                     y_V_E::Float64, κsoma::Float64)::NTuple{2,Float64}
+function rate_bracket(ureserve::NTuple{3}, A_turnover::NTuple{3}, 
+                     j_E_mai, y_E_CH_NO, y_E_EN, y_V_E, κsoma)::NTuple{2}
     # Calculate the limits of the rate_formula function when C or N → ∞
     (uEC, uEN, uE) = ureserve
     (AEC, AEN, AE) = A_turnover
@@ -96,48 +31,31 @@ function rate_window(ureserve::NTuple{3,Float64}, A_turnover::NTuple{3,Float64},
 end
 
 """
+    rate_formula(r, ureserve::NTuple, A_turnover::NTuple, j_E_mai, y_V_E, κsoma)
 Rate formulas for E, CN or CNE reserves
 """
-function rate_formula(r::Float64, ureserve::NTuple{1,Float64}, A_turnover::NTuple{1,Float64}, 
-                      j_E_mai::Float64, y_V_E::Float64, κsoma::Float64)::Float64
-    j_E = catabolic_fluxes(ureserve, A_turnover, r)
-    return y_V_E * (κsoma * j_E - j_E_mai) - r
-end
-function rate_formula(r::Float64, ureserve::NTuple{2,Float64}, A_turnover::NTuple{2,Float64}, 
-                      j_E_mai::Float64, y_E_CH_NO::Float64, y_E_EN::Float64, 
-                      y_V_E::Float64, κsoma::Float64)::Float64
-    (j_EC, j_EN) = catabolic_fluxes(ureserve, A_turnover, r)
-    j_ECN = stoich_merge(j_EC * y_E_CH_NO, j_EN * y_E_EN)  
-    return y_V_E * (κsoma * j_ECN - j_E_mai) - r
-end
-function rate_formula(r::Float64, ureserve::NTuple{3,Float64}, A_turnover::NTuple{3,Float64}, 
-                      j_E_mai::Float64, y_E_CH_NO::Float64, y_E_EN::Float64, 
-                      y_V_E::Float64, κsoma::Float64)::Float64
+function rate_formula(r, ureserve::NTuple{3}, A_turnover::NTuple{3}, 
+                      j_E_mai, y_E_CH_NO, y_E_EN, y_V_E, κsoma)
     (j_EC, j_EN, j_E) = catabolic_fluxes(ureserve, A_turnover, r)
     j_ECN = stoich_merge(j_EC * y_E_CH_NO, j_EN * y_E_EN)  
     return y_V_E * (κsoma * (j_E + j_ECN) - j_E_mai) - r
 end
 
 """
+    catabolic_fluxes(ureserve, A_turnover, r)
 Returns the current catabolic flux at rate r,
 or the flux as a proportion of u[V], depending on ureserve values.
 """
-function catabolic_fluxes(ureserve::NTuple{N,Float64}, A_turnover::NTuple{N,Float64}, 
-                          r::Float64)::NTuple{N,Float64} where N
-    ureserve .* (A_turnover .- r)
-end
+catabolic_fluxes(ureserve, A_turnover, r) = ureserve .* (A_turnover .- r)
 
 ###############################################
 # Stoichiometry
-
 
 """
     half_saturation(x, half, max)
 Half satration curve.
 """
-function half_saturation(x, half, max)
-    max/(1.0 + half/x) 
-end 
+half_saturation(max, half, x) = max/(oneunit(half/x) + half/x) 
 
 """
     stoich_merge(a, b)
@@ -145,28 +63,16 @@ Merge two inputs stoichiometrically. The minimum value is limiting,
 and stochasticity of pairing is simulated so that for any a, b 
 stoich_merge(a, b) < a, to the limit b → ∞ where stoich_merge(a, b) = a   
 """
-function stoich_merge(a::Float64, b::Float64)::Float64
-    1.0/(1.0/a + 1.0/b - 1.0/(a + b))
-end
+stoich_merge(a, b) = 1.0/(1.0/a + 1.0/b - 1.0/(a + b))
 
 """
-    synthesizing_unit(Ja, Jb, ya, yb)
-Merge fluxes stoichiometrically into general reserve Eab
-The remainder is returned as unmixed reserves Ea and Eb
+    synthesizing_unit(Ja, Jb, ya, yb) 
+Merge fluxes stoichiometrically into general reserve Eab based on yeild 
+fractions ya and yb. The remainder is returned as unmixed reserves Ea and Eb.
 """
-function synthesizing_unit(Ja::Float64, Jb::Float64, ya::Float64, yb::Float64
-                          )::NTuple{3,Float64} 
+synthesizing_unit(Ja, Jb, ya, yb) = begin
     JEab = stoich_merge(Ja * ya, Jb * yb) 
     JEa = Ja - JEab/ya                        
     JEb = Jb - JEab/yb                     
     (JEa, JEb, JEab)
 end
-
-"""
-Check if germination has happened. Independent for each structures,
-although this may not make sense.
-"""
-function germinated(M_V, M_Vgerm::Float64)::Bool 
-    M_V > M_Vgerm 
-end
-
