46a9b162460d82b6edfe4ec14f1922c89dfa7b8c
Rafael Schouten <rafaelschouten@gmail.com>
2018-06-19 03:38:46 +1000

    fix environment

--- src/environment.jl
+++ src/environment.jl
@@ -1,20 +1,20 @@
 const water_fraction_to_M = 1.0u"m^3*m^-3" * 1u"kg*L^-1" / 18.0u"g*mol^-1"
 
-get_environment(::Type{Val{:soiltemperature}}, env::MicroclimateData, h, i) = 
+get_environment(::Type{Val{:soiltemperature}}, env::MicroclimateData, interp, i) = 
     layer_interpolate(interp, env.soil, i) * u"°C"
-get_environment(::Type{Val{:watercontent}}, env::MicroclimateData, h, i) =
+get_environment(::Type{Val{:watercontent}}, env::MicroclimateData, interp, i) =
     layer_interpolate(interp, env.soilmoist, i)
-get_environment(::Type{Val{:waterpotential}}, env::MicroclimateData, h, i) = 
+get_environment(::Type{Val{:waterpotential}}, env::MicroclimateData, interp, i) = 
     layer_interpolate(interp, env.soilmoist, i) * u"Pa"
-get_environment(::Type{Val{:airtemperature}}, env::MicroclimateData, h, i) =
+get_environment(::Type{Val{:airtemperature}}, env::MicroclimateData, interp, i) =
     lin_interpolate(env.metout[:TALOC], i) * u"°C"
-get_environment(::Type{Val{:windspeed}}, env::MicroclimateData, h, i) =
+get_environment(::Type{Val{:windspeed}}, env::MicroclimateData, interp, i) =
     lin_interpolate(env.metout[:VLOC], i) * u"m*s^-1"
-get_environment(::Type{Val{:relhumidity}}, env::MicroclimateData, h, i) =
+get_environment(::Type{Val{:relhumidity}}, env::MicroclimateData, interp, i) =
     layer_interpolate(interp, env.humid, i)
-get_environment(::Type{Val{:radition}}, env::MicroclimateData, h, i) =
+get_environment(::Type{Val{:radition}}, env::MicroclimateData, interp, i) =
     lin_interpolate(env.metout[:SOLR], i) * u"W*m^-2"
-get_environment(::Type{Val{:par}}, env::MicroclimateData, h, i) =
+get_environment(::Type{Val{:par}}, env::MicroclimateData, interp, i) =
     lin_interpolate(env.metout[:SOLR], i) * 4.57u"mol*m^-2*s^-1"
 
 
@@ -27,15 +27,15 @@ apply_environment!(o, a::AbstractCarbonAssimilation, env, t) = begin
     p, v, u = components(o); va = v.assimilation;
     pos = ustrip(t) + 1
     h = v.height = allometric_height(p.allometry, o)
-    interp = layer_setup(o.vars.height)
+    interp = layer_setup(v.height)
 
-    va.tair = get_environment(Val{:airtemperature}, env, h, pos)
-    va.windspeed = get_environment(Val{:windspeed}, env, h, pos)
-    va.rh = get_environment(Val{:relhumidity}, env, h, pos)
-    va.rnet = get_environment(Val{:radiation}, env, h, pos)
-    va.par = get_environment(Val{:par}, env, h, pos)
-    va.soilmoist = get_environment(Val{:soilwatercontent}, env, h, pos)
-    va.swp = get_environment(Val{:soilwaterpotential}, env, h, pos)
+    va.tair = get_environment(Val{:airtemperature}, env, interp, pos)
+    va.windspeed = get_environment(Val{:windspeed}, env, interp, pos)
+    va.rh = get_environment(Val{:relhumidity}, env, interp, pos)
+    va.rnet = get_environment(Val{:radiation}, env, interp, pos)
+    va.par = get_environment(Val{:par}, env, interp, pos)
+    va.soilmoist = get_environment(Val{:soilwatercontent}, env, interp, pos)
+    va.swp = get_environment(Val{:soilwaterpotential}, env, interp, pos)
 
     if germinated(u.V, p.M_Vgerm)
         phototranspiration!(va, va.photoparams)
@@ -50,9 +50,10 @@ apply_environment!(o, a::KooijmanSLAPhotosynthesis, env, t) = begin
     p, v, u = components(o); va = v.assimilation;
     pos = ustrip(t) + 1
     h = v.height = allometric_height(p.allometry, o)
+    interp = layer_setup(v.height)
 
-    va.tair = get_environment(Val{:airtemperature}, env, h, pos)
-    va.J_L_F = get_environment(Val{:par}, env, h, pos)
+    va.tair = get_environment(Val{:airtemperature}, env, interp, pos)
+    va.J_L_F = get_environment(Val{:par}, env, interp, pos)
     correct_temps!(o, va.tair)
 end
 
@@ -61,8 +62,9 @@ apply_environment!(o, a::AbstractNitrogenAssimilation, env, t) = begin
     pos = ustrip(t) + 1
     h = v.height = allometric_height(p.allometry, o)
     interp = layer_setup(v.height)
-    va.temp = get_environment(Val{:soiltemperature}, env, h, pos)
-    va.X_H = get_environment(Val{:soilwatercontent}, env, h, pos) * water_fraction_to_M
+
+    va.temp = get_environment(Val{:soiltemperature}, env, interp, pos)
+    va.X_H = get_environment(Val{:soilwatercontent}, env, interp, pos) * water_fraction_to_M
 
     correct_temps!(o, va.temp)
 end
